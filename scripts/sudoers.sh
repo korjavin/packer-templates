ff836b7fdfc07e0efa900ea3ba7ab2c73add11a0
fh <fh-github@fholzhauer.de>
2013-07-10 15:04:45 +0200

    fixing sudoers access

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -2,3 +2,6 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e 's/%root ALL=(ALL) ALL/%root ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+usermod -a -G root vagrant
