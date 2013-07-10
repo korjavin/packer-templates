0e1fd730993dbdd59ace3ccd641e1a5166ace19a
fh <fh-github@fholzhauer.de>
2013-07-10 15:16:13 +0200

    now works

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -2,6 +2,7 @@
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
-sed -i -e 's/%root ALL=(ALL) ALL/%root ALL=NOPASSWD:ALL/g' /etc/sudoers
+echo %root ALL=NOPASSWD:ALL>/etc/sudoers.d/root-nopasswd
+chmod 0440 /etc/sudoers.d/root-nopasswd
 
 usermod -a -G root vagrant
